8d4aef9a0345e2e4f389f8050fdafa430ea91da9
chunis <chunchengfh@gmail.com>
2024-07-28 11:41:18 +0800

    port to esp32

--- interface.py
+++ interface.py
@@ -5,18 +5,32 @@
 
 import time
 from machine import SoftSPI
-from pyb import Pin
 from utils import *
+
+import sys
+if sys.platform == 'pyboard':
+    from pyb import Pin
+elif sys.platform == 'esp32':
+    from machine import Pin
 
 
 class SPILink:
     def __init__(self):
-        self.nss   = Pin(Pin.board.PA8, Pin.OUT, value=1)
-        self.reset = Pin(Pin.board.PA0, Pin.OUT, value=1)
-        self.irq   = Pin(Pin.board.PB4, Pin.IN)
-        self.busy  = Pin(Pin.board.PB3, Pin.IN)
-        self.spi   = SoftSPI(baudrate=1000000, polarity=0, phase=0, bits=8,
-                      sck=Pin.board.PA5, mosi=Pin.board.PA7, miso=Pin.board.PA6)
+        if sys.platform == 'pyboard':
+            self.nss   = Pin(Pin.board.PA8, Pin.OUT, value=1)
+            self.reset = Pin(Pin.board.PA0, Pin.OUT, value=1)
+            self.irq   = Pin(Pin.board.PB4, Pin.IN)
+            self.busy  = Pin(Pin.board.PB3, Pin.IN)
+            self.spi   = SoftSPI(baudrate=1000000, polarity=0, phase=0, bits=8,
+                          sck=Pin.board.PA5, mosi=Pin.board.PA7, miso=Pin.board.PA6)
+        elif sys.platform == 'esp32':
+            self.nss   = Pin(8, Pin.OUT, value=1)
+            self.reset = Pin(12, Pin.OUT, value=1)
+            self.irq   = Pin(14, Pin.IN)
+            self.busy  = Pin(13, Pin.IN)
+            self.spi   = SoftSPI(baudrate=1000000, polarity=0, phase=0, bits=8,
+                          sck=Pin(9), mosi=Pin(10), miso=Pin(11))
+
 
     def send_packet(self, data, ret_size):
         self.wait_busy(0)
